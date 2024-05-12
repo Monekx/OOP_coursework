1573b6e004835e328a086f4187bd90c191693fd5
Максим Соболев <sobolutcom183@gmail.com>
2024-05-12 12:13:43 +0300

    Some minor updates

---
+++ Artist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_coursework
+{
+    internal class Artist
+    {
+
+        private static int NextID = 1;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Artist(string Name) 
+        {
+            this.Name = Name;
+            Id = NextID++;
+        }
+
+
+
+    }
+}
